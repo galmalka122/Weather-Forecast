7b4c58c724b176adccadf0f74a6bef3a6856ad8f
Gal Malka <galmalka122@gmail.com>
2022-04-18 17:34:58 +0300

    first touch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Form from "./components/Form";
 
 function App() {
+  const [result, setResult] = useState("");
+  const [imgResult, setImageResult] = useState("");
+
+  function receiveResultFromServer(result) {
+    setResult(result);
+  }
+
+  function receiveImageResultFromServer(result) {
+    setImageResult(result);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        url={"https://www.7timer.info/bin/api.pl"}
+        receiveResult={receiveResultFromServer}
+        reciveImageResult={receiveImageResultFromServer}
+      />
+      {result ? (
+        <div className="border p-3">Result is {JSON.stringify(result)}</div>
+      ) : (
+        ""
+      )}
+      {imgResult ? <img src={imgResult} alt="Weather Visual"></img> : ""}
     </div>
   );
 }
